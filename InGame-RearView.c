254f84c111c520acdac8aa6cd06914e072202be8
HootHoot64 <76700023+HootHoot64@users.noreply.github.com>
2021-05-02 13:36:37 +0200

    Update InGame-RearView.c

--- InGame-RearView.c
+++ InGame-RearView.c
@@ -1,21 +1,24 @@
-//REARVIEW V1------------------------------------------------------------------------------------
+//REARVIEW V1 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void rearView(void)
 {
 
-    //loadFont();
-    //printStringNumber(10,10, "view:", GlobalScreen[0]->camera_direction);
-    //printStringNumber(10,20, "Camdir:", GlobalCamera[0]->chase_direction);
+    /*
+    //DEBUG
+    loadFont();
+    printStringNumber(10,10, "view:", GlobalScreen[0]->camera_direction);
+    printStringNumber(10,20, "Camdir:", GlobalCamera[0]->chase_direction);
+    */
 
     static int tempDir[4];
-    int i;    
+    int i; 
+    
     for(i=0; i<4; i++) 
         {   
             switch(GlobalController[i]->ButtonPressed)
             {
                 case (BTN_DDOWN):
                 {   
-                    //loadFont();
-                    //printStringNumber(10,30, "pressed", GlobalScreen[0]->camera_direction);
+                    //loadFont(); printStringNumber(10,30, "pressed", GlobalScreen[0]->camera_direction);
                     tempDir[i] = GlobalCamera[i]->chase_direction;
                     break;
                 }
@@ -27,10 +30,7 @@ void rearView(void)
                 case (BTN_DDOWN+BTN_A):
                 case (BTN_DDOWN+BTN_A+BTN_Z):
                 {   
-                    //loadFont();
-                    //printStringNumber(10,40, "held", GlobalScreen[0]->camera_direction);
-                    //invert steering?
-
+                    //loadFont(); printStringNumber(10,40, "held", GlobalScreen[0]->camera_direction);
                     if(tempDir[i] == -32768){GlobalCamera[i]->chase_direction = 0;break;}
                     else if(tempDir[i] >= 0){GlobalCamera[i]->chase_direction = tempDir[i] - 32767;}
                     else{GlobalCamera[i]->chase_direction = tempDir[i] + 32767;};
